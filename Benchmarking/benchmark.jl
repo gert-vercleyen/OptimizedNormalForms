90d8473304024d43cd948d2b71ca324dde42f159
gert-vercleyen <gert.vercleyen@protonmail.com>
2024-11-18 17:54:30 -0500

    Update benchmark.jl

--- Benchmarking/benchmark.jl
+++ Benchmarking/benchmark.jl
@@ -2,16 +2,20 @@ using Oscar
 using DelimitedFiles
 using TimerOutputs
 
-const to = TimerOutput()
+to = TimerOutput()
 
 linuxFileNames = readdlm("Benchmarking/FileNamesLinux",'\n',String)
 
 numMatrices = 30
 
-for i in 1:numMatrices
-  mat = matrix( ZZ, readdlm( "JULIA/" * linuxFileNames[i] ) );
-  println(i);
-  @timeit to linuxFileNames[i]*"_" snf_with_transform(mat);
-end
+# for i in 1:numMatrices
+#   mat = matrix( ZZ, readdlm( "JULIA/" * linuxFileNames[i] ) );
+#   println(i);
+#   @timeit to linuxFileNames[i] snf_with_transform(mat);
+# end
 
-show(to)
+#show(to)
+
+@time mat = matrix( ZZ, readdlm( "JULIA/" * linuxFileNames[30] ) );
+
+@profview_allocs snf_with_transform(mat) sample_rate=0.1
